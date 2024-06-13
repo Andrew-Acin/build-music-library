cada3caf8160238c01bd58596011621471cd3a5c
Andrew Acin <andrew_acin@hotmail.com>
2024-06-12 21:05:04 -0700

    finished code along 5

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { useEffect, useState, useRef } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Gallery from './components/Gallery';
 import SearchBar from './components/SearchBar';
-import { DataContext } from './context/DataContext';
-import { SearchContext } from './context/SearchContext';
+import AlbumView from './components/AlbumView';
+import ArtistView from './components/ArtistView';
+// import { DataContext } from './context/DataContext';
+// import { SearchContext } from './context/SearchContext';
 import './App.css';
 
 function App() {
   let [message, setMessage] = useState('Serach for Music!')
   let [data, setData] = useState([])
-  let searchInput = useRef('')
+  // let searchInput = useRef(null)
 
   const API_URL = 'https://itunes.apple.com/search?term='
 
@@ -30,18 +33,22 @@ const handleSearch = (e, term) => {
 
   return (
     <div className="App">
-        <SearchContext.Provider value={{
-            term: searchInput,
-            handleSearch: handleSearch
-          }}>
-          <SearchBar />
-        </SearchContext.Provider>
-        {message}
-        <DataContext.Provider value={data}>
-          <Gallery />
-        </DataContext.Provider>
+      {message}
+      <Router>
+        <Routes>
+          <Route path="/" element={
+            <>
+              <SearchBar handleSearch={handleSearch} />
+              <Gallery data={data} />
+            </>
+          } />
+          <Route path="/album/:id" element={<AlbumView />} />
+          <Route path="/artist/:id" element={<ArtistView />} />
+        </Routes>
+      </Router>
     </div>
-  );
+  )
+
 }
 
 export default App;
